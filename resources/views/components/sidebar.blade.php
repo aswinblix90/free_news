d42a408ebad2065b0cb6f6edbe3239becb06d233
aswinblix <aswin.blix@bitsnpixs.com>
2022-09-06 17:31:00 +0530

    add layout and home page

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,25 @@
+<div class="sidebar">
+    <div class="widget">
+        <h2 class="widget-title">Search</h2>
+        <x-search></x-search>
+    </div><!-- end widget -->
+
+    <div class="widget">
+        <h2 class="widget-title">Recent Posts</h2>
+        <div class="blog-list-widget">
+            <div class="list-group">
+                <x-sidebar-recent-post/>
+                <x-sidebar-recent-post/>
+                <x-sidebar-recent-post/>
+                <x-sidebar-recent-post/>
+            </div>
+        </div><!-- end blog-list -->
+    </div><!-- end widget -->
+
+    <div class="widget">
+        <h2 class="widget-title">Popular Categories</h2>
+        <div class="link-widget">
+            <x-categories-listing/>
+        </div><!-- end link-widget -->
+    </div><!-- end widget -->
+</div><!-- end sidebar -->
