0ae98741e03212eeff8dffe4686b0fe05ff0e6d9
aswinblix <aswin.blix@bitsnpixs.com>
2022-09-07 11:56:03 +0530

    display post from table

--- resources/views/components/categories-listing.blade.php
+++ resources/views/components/categories-listing.blade.php
@@ -1,7 +1,6 @@
+@props(['categories'])
 <ul>
-    <li><a href="#">Gardening <span>(21)</span></a></li>
-    <li><a href="#">Outdoor Living <span>(15)</span></a></li>
-    <li><a href="#">Indoor Living <span>(31)</span></a></li>
-    <li><a href="#">Shopping Guides <span>(22)</span></a></li>
-    <li><a href="#">Pool Design <span>(66)</span></a></li>
+    @foreach ($categories as $category)
+        <li><a href="#">{{$category->name}} <span>({{$category->posts_count}})</span></a></li>
+    @endforeach
 </ul>
