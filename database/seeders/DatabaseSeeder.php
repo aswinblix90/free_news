f345e96ecfa2769e91fc017ee536a6528f3380a4
aswinblix <aswin.blix@bitsnpixs.com>
2022-09-07 10:08:11 +0530

    add table with seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +26,34 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $roles = ['Admin','User'];
+        $categories = ['Personal', 'Work', 'Hobbies'];
+        foreach($roles as $role){
+            Role::create([
+                'name' => $role
+            ]);
+        }
+        // foreach ($categories as $category) {
+        //     Category::create([
+        //         'name' => $category,
+        //         'slug' => Str::slug($category)
+        //     ]);
+        // }
+        // Role::created([
+        //     'name' => $role
+        // ]);
+        Post::factory(5)->create();
+        Comment::factory(2)->create([
+            'post_id' => 1,
+            'user_id' => 2
+        ]);
+        // Comment::factory()->create([
+        //     'post_id' => 1,
+        //     'user_id' => 2
+        // ]);
+        Comment::factory(2)->create([
+            'post_id' => 3,
+            'user_id' => 4
+        ]);
     }
 }
